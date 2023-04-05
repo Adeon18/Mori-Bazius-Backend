11b3330edb9f221d1899f04e6f36061833a994f7
Adeon18 <ostaptrush18@gmail.com>
2023-04-05 22:19:35 +0300

    NOW IT SHOULD FKIN DIE

--- app/tests/run-tests.sh
+++ app/tests/run-tests.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
 echo "Running tests..."
+
+all_passed=0
+
 for tst in $(ls test_*.py)
 do
-    coverage run -p "${tst}"   
+    coverage run -p "${tst}"
+
+    if [ $? -eq 1 ]; then
+        all_passed=1
+    fi
 done
+
+if [ "${all_passed}" -eq 1 ]; then
+    exit 1
+fi
 
 echo "Combining results..."
 coverage combine
