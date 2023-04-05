9f450507959850e806360db2e9639be9f732727a
Adeon18 <ostaptrush18@gmail.com>
2023-04-05 22:08:20 +0300

    FUCKING ALRIGHT

--- app/tests/run-tests.sh
+++ app/tests/run-tests.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+echo "Running tests..."
 for tst in $(ls test_*.py)
 do
     coverage run -p "${tst}"   
 done
 
+echo "Combining results..."
 coverage combine
+
+echo "Results:"
+coverage report
