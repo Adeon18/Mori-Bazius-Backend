745f450211fa323e75accc43827db252574e271c
Adeon18 <ostaptrush18@gmail.com>
2023-06-05 17:36:42 +0300

    Silly little shit in main

--- app/game_data_service/service/game_data_service.py
+++ app/game_data_service/service/game_data_service.py
@@ -24,8 +24,8 @@ class GameDataService:
                                                    port=8000, check=check)
         
         self.event_loop = asyncio.get_event_loop()
-        self.data_consumer = AIOKafkaConsumer("game-data", loop=self.event_loop, bootstrap_servers=kafka_address)
-        self.stats_consumer = AIOKafkaConsumer("game-stats", loop=self.event_loop, bootstrap_servers=kafka_address)
+        self.data_consumer = AIOKafkaConsumer("game-data", loop=self.event_loop, bootstrap_servers=kafka_address, group_id="game_data_consumer", auto_offset_reset="earliest", enable_auto_commit=True)
+        self.stats_consumer = AIOKafkaConsumer("game-stats", loop=self.event_loop, bootstrap_servers=kafka_address, group_id="game_data_consumer", auto_offset_reset="earliest", enable_auto_commit=True)
     
     def new_service_with_cassandra():
         return GameDataService(CassandraRepository())
